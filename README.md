f12f625d1ee4f030e2138d6f2d4729f5f6826e31
Will Tirone <will.tirone1@gmail.com>
2021-04-19 15:14:38 -0400

    updating readme, adding confi and setup

--- README.md
+++ README.md
@@ -2,20 +2,25 @@
 Creating a project to implement various statistical distributions and methods in Python.
 
 To install and run this project, do the following:
+1. Run the following to install: 
 
-1. Clone the repository to your machine using
-```git clone https://github.com/WillTirone/Statistics_Module.git```
+```python 
+pip install stats_tools 
+```
 
-2. install the dependencies
-```pip install -r requirements.txt```
-- alternatively, use the [Anaconda installation](https://www.anaconda.com/) of Python 
+    Or, clone the repo directly: 
+    a). Clone the repository to your machine using
+    ```git clone https://github.com/WillTirone/Statistics_Module.git```
+    
 
 3. Follow these examples to start plotting and calculating probabilities. The examples below, and several others, can also be found in the  [Demonstration Jupyter Notebook](https://github.com/WillTirone/Statistics_Module/blob/main/demonstrations/Demonstration.ipynb)
 
 4. To run the test file, from the command line enter: ```python test.py```
 
+## Usage
+
 ```python
->>> from Statistics_OOP import *
+>>> from stats_tools import *
 
 >>> a = Norm_rv(0,1)
 >>> a.plot_pdf()
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+      name="stats_tools",
+      version='0.0.1',
+      description="A basic statistics module to compute MLEs / probabilities",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=find_packages(where='stats_tools'), #instead of py_modules? 
+      package_dir={'':"stats_tools"},
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Statistics',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        "Operating System :: OS Independent"
+        ],
+      install_requires = ['scipy ~= 1.3.0', 'numpy ~= 1',]
+      )
